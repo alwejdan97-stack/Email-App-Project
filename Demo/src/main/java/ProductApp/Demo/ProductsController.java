f33ea084d2cfe027fc245ceebbe980afb9321ddf
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 12:49:39 +0400

    creating ProductsController class and adding required annotations

---
+++ Demo/src/main/java/ProductApp/Demo/ProductsController.java
@@ -0,0 +1,9 @@
+package ProductApp.Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class ProductsController {
+}
