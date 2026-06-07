05b726cfe2a09f744dff6553d23e3e1adf35ee62
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 11:58:07 +0400

    creating userProfile object

--- Demo/src/main/java/EmailApp/Demo/UserProfileController.java
+++ Demo/src/main/java/EmailApp/Demo/UserProfileController.java
@@ -1,4 +1,15 @@
 package EmailApp.Demo;
 
 public class UserProfileController {
+    public static void main(String[] args){
+        UserProfile userProfile=new UserProfile("U1-01","98765432","Oman");
+
+        System.out.println("*** Current Information ***");
+        System.out.println("User ID: "+userProfile.getUserId());
+        System.out.println("User Phone Number: "+userProfile.getPhoneNumber());
+        System.out.println("User Address: "+userProfile.getAddress());
+
+        System.out.println("*** Updating Phone Number AND Address ***");
+
+    }
 }
