cccdd0ce0d215647644d37a2f712ac6edbb7ddc3
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 14:19:37 +0400

    add a constructor to initialize all fields

--- Demo/src/main/java/ProductApp/Demo/Order.java
+++ Demo/src/main/java/ProductApp/Demo/Order.java
@@ -4,4 +4,10 @@ public class Order {
     private int orderId;
     private String shippingAddress;
     private String orderStatus;
+
+    public Order(int orderId, String shippingAddress, String orderStatus) {
+        this.orderId = orderId;
+        this.shippingAddress = shippingAddress;
+        this.orderStatus = orderStatus;
+    }
 }
