fce9dc18bfd51a0153cf595862c7e084d2bcc798
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 11:48:06 +0400

    Create a constructor that initializes all fields when a UserProfile object is created

--- Demo/src/main/java/EmailApp/Demo/UserProfile.java
+++ Demo/src/main/java/EmailApp/Demo/UserProfile.java
@@ -4,4 +4,12 @@ public class UserProfile {
     private String userId;
     private String phoneNumber;
     private String address;
+
+    public UserProfile(String userId, String phoneNumber, String address) {
+        this.userId = userId;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+
 }
