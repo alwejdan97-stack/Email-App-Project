56b60a60ba7ba6bcfa21b9d0172d15a088de9a49
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 11:45:35 +0400

    adding new code

--- Demo/src/main/java/EmailApp/Demo/DemoApplication.java
+++ Demo/src/main/java/EmailApp/Demo/DemoApplication.java
@@ -7,20 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(DemoApplication.class, args);
-		String currentEmailAddress="wejdan@gmail.com";
-		String newEmailAddress="ali@gmail.com";
-		boolean status=false;
-
-		System.out.println("The current Email is: "+currentEmailAddress);
-
-		if(!currentEmailAddress.equalsIgnoreCase(newEmailAddress)){
-			currentEmailAddress=newEmailAddress;
-			status=true;
-			System.out.println("Email Address Updated SUCCESSFULLY...");
-		}else{
-			System.out.println("NEW Email Address Updated SUCCESSFULLY...");
-		}
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- Demo/src/main/java/EmailApp/Demo/UpdateEmailDemo.java
+++ Demo/src/main/java/EmailApp/Demo/UpdateEmailDemo.java
@@ -1,4 +1,24 @@
 package EmailApp.Demo;
-
+import java.util.*;
 public class UpdateEmailDemo {
+    public static void main(String[] args){
+        String currentEmailAddress="wejdan@gmail.com";
+        String newEmailAddress="wejdan@gmail.com";
+        boolean status=false;
+
+        System.out.println("The current Email is: "+currentEmailAddress);
+
+        if(!currentEmailAddress.equalsIgnoreCase(newEmailAddress)){
+            currentEmailAddress=newEmailAddress;
+            status=true;
+            System.out.println("Email Address Updated SUCCESSFULLY...");
+            System.out.println("Updated Email Address Is: "+currentEmailAddress);
+            System.out.println("Updated Status "+status);
+        }else{
+            status=false;
+            System.out.println("NO Update Required, NEW Email Address Matched Old Email...");
+            System.out.println("Updated Email Address Is: "+currentEmailAddress);
+            System.out.println("Updated Status "+status);
+        }
+    }
 }
