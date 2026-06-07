40bde991c1a2cbe0595a54c89a921c3f6a2ab7d6
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 12:42:18 +0400

    add a constructor to initialize all fields

--- Demo/src/main/java/ProductApp/Demo/Product.java
+++ Demo/src/main/java/ProductApp/Demo/Product.java
@@ -4,4 +4,10 @@ public class Product {
     private String id;
     private String name;
     private int stockQuantity;
+
+    public Product(String id, String name, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+    }
 }
