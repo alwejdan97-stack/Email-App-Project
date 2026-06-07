83ff6d44daa59082b3491d80e866dbb91e9a0306
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 11:48:52 +0400

    Create setter and getter medthods

--- Demo/src/main/java/EmailApp/Demo/UserProfile.java
+++ Demo/src/main/java/EmailApp/Demo/UserProfile.java
@@ -11,5 +11,27 @@ public class UserProfile {
         this.address = address;
     }
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
